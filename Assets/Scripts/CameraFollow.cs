eef916e8025cad7c9c07bb5d86fe1af52670af8c
Brandon Lacy <blacy1551@gmail.com>
2019-11-12 14:31:01 -0600

    Added Basic Player
    1) Player Movement
    2) Player Animations
    3) Player Shooting

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    public Vector3 lookPos;
+    private void Update()
+    {
+        transform.LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = desiredPosition;
+
+    }
+
+}
