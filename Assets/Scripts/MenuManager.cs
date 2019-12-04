4e708bc3d75cae22324747c9e7b6264732a788bb
jmpinoy <joshuamiranda@unomaha.edu>
2019-12-03 19:15:01 -0600

    MainMenu
    Main Menu created

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject TitlePanel;
+    [SerializeField] private GameObject OptionPanel;
+
+
+    void Awake()
+    {
+        OptionPanel.SetActive(false);
+    }
+
+    public void SwitchPanels(int panel)
+    {
+        TitlePanel.SetActive((panel == 0));
+        OptionPanel.SetActive((panel == 1));
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnVolumeChange()
+    {
+        Debug.Log("Volume Changed");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
