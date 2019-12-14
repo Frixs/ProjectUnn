92a8db66eff96be3030182b86b043e05f7aa70e3
jmpinoy <joshuamiranda@unomaha.edu>
2019-12-14 17:09:38 -0600

    Menu
    title screen, options menu, how to play screen

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -7,22 +7,50 @@ public class MenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject TitlePanel;
     [SerializeField] private GameObject OptionPanel;
+    [SerializeField] private GameObject TutorialPanel;
+    [SerializeField] private GameObject[] TutorialPanels;
+    private int currentTutorialPanel;
 
 
     void Awake()
     {
-        OptionPanel.SetActive(false);
+        currentTutorialPanel = 0;
+        SwitchPanels(0);
+        ApplyTutorialPanels();
     }
 
     public void SwitchPanels(int panel)
     {
         TitlePanel.SetActive((panel == 0));
         OptionPanel.SetActive((panel == 1));
+        TutorialPanel.SetActive((panel == 2));
     }
 
     public void Play()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    public void IncrementTutorialPanels()
+    {
+        currentTutorialPanel = Mathf.Min(currentTutorialPanel + 1, TutorialPanels.Length - 1);
+        ApplyTutorialPanels();
+    }
+
+    public void DecrementTutorialPanels()
+    {
+        currentTutorialPanel = Mathf.Max(currentTutorialPanel - 1, 0);
+        ApplyTutorialPanels();
+    }
+
+    public void ApplyTutorialPanels()
+    {
+        foreach(GameObject panel in TutorialPanels)
+        {
+            panel.SetActive(false);
+        }
+
+        TutorialPanels[currentTutorialPanel].SetActive(true);
     }
 
     public void OnVolumeChange()
